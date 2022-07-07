f20d45c209cb92949decf38400ed48480f403dfc
Dmitry Kashtanov <Dmitry.Kashtanov@piano.io>
2022-07-07 20:01:14 +0300

    Made QuickStart work in `python` client folder.

--- clients/python/my_strategy.py
+++ clients/python/my_strategy.py
@@ -1,6 +1,4 @@
-from model.game import Game
-from model.order import Order
-from model.constants import Constants
+from model import Game, Constants, Order, Vec2, ActionOrder, UnitOrder
 from typing import Optional
 from debug_interface import DebugInterface
 
@@ -8,9 +6,23 @@ from debug_interface import DebugInterface
 class MyStrategy:
     def __init__(self, constants: Constants):
         pass
+
     def get_order(self, game: Game, debug_interface: Optional[DebugInterface]) -> Order:
-        return Order({})
+        orders = {}
+        for unit in game.units:
+            if unit.player_id != game.my_id:
+                continue
+            orders[unit.id] = UnitOrder(
+                Vec2(-unit.position.x, -unit.position.y),
+                Vec2(-unit.direction.y, unit.direction.x),
+                ActionOrder.Aim(True),
+            )
+        order = Order(orders)
+        print(order)
+        return order
+
     def debug_update(self, displayed_tick: int, debug_interface: DebugInterface):
         pass
+
     def finish(self):
         pass
